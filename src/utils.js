ae9a9e8ec7d35f4faa967a85d1e9da9279379216
keka <kvtka4enko@yandex.ru>
2025-02-20 17:17:22 +0300

    offers fixes

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,10 @@ import dayjs from 'dayjs';
 export function humanizeEventDate(date, format) {
   return (date) ? dayjs(date).format(format) : '';
 }
+
+export const handleEscapeKeyPress = (evt, onEscape) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    onEscape();
+  }
+};
